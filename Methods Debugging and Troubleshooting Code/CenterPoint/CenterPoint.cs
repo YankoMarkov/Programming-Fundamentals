eed9b572aa68c9361a9c2ad49ce5c91bbef5a5dc
Yanko Markov <yanko@markov.eu>
2017-07-14 21:54:12 +0300

    Add files via upload

---
+++ Methods Debugging and Troubleshooting Code/CenterPoint/CenterPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CenterPoint
+{
+    public static void Main(string[] args)
+    {
+        var x1 = double.Parse(Console.ReadLine());
+        var y1 = double.Parse(Console.ReadLine());
+        var x2 = double.Parse(Console.ReadLine());
+        var y2 = double.Parse(Console.ReadLine());
+
+        CloserPointDistance(x1, y1, x2, y2);
+    }
+
+    public static void CloserPointDistance(double x1, double y1, double x2, double y2)
+    {
+        var x = 0.0;
+        var y = 0.0;
+
+        if (Math.Pow(x1, 2) + Math.Pow(y1, 2) <= Math.Pow(x2, 2) + Math.Pow(y2, 2))
+        {
+            x = x1;
+            y = y1;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+        }
+
+        Console.WriteLine($"({x}, {y})");
+    }
+}
+
