d8deeebdcabe554841dff32d6134ca79d282580f
Yanko Markov <yanko@markov.eu>
2017-07-14 19:24:01 +0300

    Add files via upload

---
+++ Arrays/MaxSequenceOfIncreasingElements/MaxSequenceOfIncreasingElements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MaxSequenceOfIncreasingElements
+{
+    public static void Main(string[] args)
+    {
+        var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var count = 0;
+        var index = 0;
+        var bestCount = 0;
+        var bestIndex = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > array[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+                index = i;
+            }
+            if (count > bestCount)
+            {
+                bestCount = count;
+                bestIndex = index;
+            }
+        }
+        
+        for (int i = bestIndex; i <= bestIndex + bestCount; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
+
---
+++ Arrays/MostFrequentNumber/MostFrequentNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MostFrequentNumber
+{
+    public static void Main(string[] args)
+    {
+        var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var bestCount = 0;
+        var result = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            var count = 0;
+            for (int j = i; j < array.Length; j++)
+            {
+                if (array[j] == array[i])
+                {
+                    count++;
+                }
+            }
+            if (count > bestCount)
+            {
+                bestCount = count;
+                result = array[i];
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
+
