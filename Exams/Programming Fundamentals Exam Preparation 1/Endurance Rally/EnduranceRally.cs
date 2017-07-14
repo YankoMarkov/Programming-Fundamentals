33acba1b55069764e075d3c2077e3981ff2ba034
Yanko Markov <yanko@markov.eu>
2017-07-14 22:59:31 +0300

    Add files via upload

---
+++ Exams/Programming Fundamentals Exam Preparation 1/Endurance Rally/EnduranceRally.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class EnduranceRally
+{
+    public static void Main()
+    {
+        var names = Console.ReadLine().Split(new[] { ' ' }).ToArray();
+        var track = Console.ReadLine().Split(new[] { ' ' }).Select(double.Parse).ToArray();
+        var checkpoint = Console.ReadLine().Split(new[] { ' ' }).Select(int.Parse).ToArray();
+
+        foreach (var name in names)
+        {
+            double fuel = name.First();
+            for (int i = 0; i < track.Length; i++)
+            {
+                if (checkpoint.Contains(i))
+                {
+                    fuel += track[i];
+                }
+                else
+                {
+                    fuel -= track[i];
+                }
+                if (fuel <= 0)
+                {
+                    Console.WriteLine($"{name} - reached {i}");
+                    break;
+                }
+            }
+            if (fuel > 0)
+            {
+                Console.WriteLine($"{name} - fuel left {fuel:F2}");
+            }
+        }
+    }
+}
+
---
+++ Exams/Programming Fundamentals Exam Preparation 1/SoftUniKaraoke/SoftUniKaraoke.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SoftUniKaraoke
+{
+    public static void Main()
+    {
+        var participants = Console.ReadLine().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray();
+        var songs = Console.ReadLine().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray();
+        var result = new Dictionary<String, List<String>>();
+
+        string input = null;
+        while (!(input = Console.ReadLine()).Equals("dawn"))
+        {
+            var inputs = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray();
+            var participant = inputs[0];
+            var song = inputs[1];
+            var award = inputs[2];
+
+            if (participants.Contains(participant) && songs.Contains(song))
+            {
+                if (!result.ContainsKey(participant))
+                {
+                    result[participant] = new List<string>();
+                }
+                if (!result[participant].Contains(award))
+                {
+                    result[participant].Add(award);
+                }
+            }
+        }
+        if (result.Any())
+        {
+            foreach (var kvp in result.OrderByDescending(e => e.Value.Count).ThenBy(e => e.Key))
+            {
+                var participant = kvp.Key;
+                var awards = kvp.Value;
+                Console.WriteLine($"{participant}: {awards.Count} awards");
+                foreach (var award in awards.OrderBy(e => e))
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("No awards");
+        }
+    }
+}
+
